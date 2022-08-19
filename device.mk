93b4c62f6a68eb3c32c278d3489806e59f0109df
daniml3 <daniel@danielml.dev>
2022-08-19 13:56:19 +0200

    courbet: Use GetPreviewImageData shim for com.qti.node.watermark
    Signed-off-by: daniml3 <daniel@danielml.dev>
    Change-Id: I92e51fdf1dc7d877c7affafbf9803ba5d504be07

--- device.mk
+++ device.mk
@@ -24,6 +24,10 @@ PRODUCT_COPY_FILES += \
 # Blur
 TARGET_ENABLE_BLUR := true
 
+# Camera
+PRODUCT_PACKAGES += \
+    libpiex_shim
+
 # Display
  PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_42_02_0b_mp_cmd_mode_dsc_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_42_02_0b_mp_cmd_mode_dsc_dsi_panel.xml \
