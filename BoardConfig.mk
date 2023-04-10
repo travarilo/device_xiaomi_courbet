1d47e6a802d25daf846e2fc48ee5e0f231544f62
Golbinex <2061409-Golbinex@users.noreply.gitlab.com>
2023-04-10 15:31:24 +0200

    courbet: Update vendor security patch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,7 +92,7 @@ TARGET_SCREEN_DENSITY := 440
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Vendor security patch level
-VENDOR_SECURITY_PATCH := 2022-09-01
+VENDOR_SECURITY_PATCH := 2023-03-01
 
 # Inherit from proprietary files
 include vendor/xiaomi/courbet/BoardConfigVendor.mk
